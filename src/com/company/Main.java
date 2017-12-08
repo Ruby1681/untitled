cf81db1d412024d4a5b782aa8227d7a94bd785ac
Law <ROS_lilaw@seattleschools.org>
2017-12-07 16:07:06 -0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+import  java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //echo();
+        City Seattle = new City(30,20);
+        //System.out.println(Seattle.popDensity(30, 20));
+       City Portland = new City(30,20);
+       Portland.plague();
+       System.out.println(Portland.population);
+       //System.out.println(Seattle.popDensity(30, 20));
+
+    }
+    public static void echo() {
+        Scanner asdf = new Scanner(System.in);
+        String line = asdf.nextLine();
+        System.out.println(line);
+    }
+    public static class City{
+        double population;
+        int size;
+
+        public City(int population, int size){
+            this.population = population;
+            this.size = size;
+
+        }
+
+        public void plague(){
+            population = population*2/3;
+        }
+        public double popDensity( int population, int size){
+            double populationDensity = population*1.0/size;
+            System.out.println(populationDensity);
+            return populationDensity;
+        }
+        public void annex(City city){
+            this.population = this.population+city.population;
+        }
+    }
+}
+
